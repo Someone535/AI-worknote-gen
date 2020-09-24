ba553e918b826bd3808db26708878971f47a8ae0
Aidan Jessen <aidan.jessen@gmail.com>
2020-09-25 02:29:14 +1000

    Add Feature: Save data, accept input and confirm
    Pop-ups now query the user for input and confirm submission of leaves.
    
    Data is saved during this process and also cleared appropriately when
    navigation occurs.

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -26,6 +26,8 @@ import css from '../css/main-page.css';
 
 import FancyButton from './fancy-button.js';
 import NavPanel from './nav-panel.js';
+import AlertPopup from './alert-popup.js';
+import InputPopup from './input-popup.js';
 
 import UI_MAP from './ui-tree.js';
 
@@ -37,6 +39,10 @@ class MainPage extends React.Component {
     this.handleHomeBtn = this.handleHomeBtn.bind(this);
     this.showContent = this.showContent.bind(this);
     this.hideContentCallback = this.hideContentCallback.bind(this);
+    this.submitCurrentLeaf = this.submitCurrentLeaf.bind(this);
+    this.goUp = this.goUp.bind(this);
+    this.saveData = this.saveData.bind(this);
+    this.navigateTo = this.navigateTo.bind(this);
 
     this.state = { path: [], data: {}, leaves: [], show_content: true };
   }; // end constructor
@@ -47,6 +53,32 @@ class MainPage extends React.Component {
     return out;
   }; // getNode
 
+  navigateTo(path) {
+    console.log(JSON.stringify(this.state));
+    // Locate data to be cleared based on differences between two paths
+    var paths_to_clear = [];
+    var path_cumulative = [];
+    var matches = true;
+    this.state.path.forEach( (el,ind) => {
+      if ( matches && ( !path[ind] || path[ind] != el ) ) matches = false;
+      path_cumulative.push(el);
+      if ( !matches ) paths_to_clear.push( path_cumulative.join(':') );
+    });
+    // Clear data and change path
+    var new_data = this.state.data;
+    paths_to_clear.forEach( el => delete this.state.data[el] );
+    // Identify values to be saved based on nodes traversed
+    var node = UI_MAP;
+    path_cumulative = [];
+    path.forEach( el => {
+      path_cumulative.push(el);
+      node = node.nodes[el];
+      if ( node.value ) new_data[ path_cumulative.join(':') ] = node.value;
+    });
+    this.setState({ path: path, data: new_data });
+    console.log(JSON.stringify(this.state));
+  }; // end navigateTo
+
   hideContentCallback( fnc, params ) {
     this.setState({ show_content: false });
     setTimeout( () => fnc(...params), 250 );
@@ -56,67 +88,140 @@ class MainPage extends React.Component {
   }; // end showContent
 
   navClick(input) {
-    console.log('nav input: '+JSON.stringify(input)+' - '+typeof input);
     if ( Array.isArray(input) ) {
       var root_path = this.state.path.slice(0,2);
-      console.log('root_path: '+root_path);
-      console.log('new_path: '+[...root_path, ...input]);
-      this.setState({ path: [...root_path, ...input ] });
+      this.navigateTo([...root_path, ...input ]);
     } else if ( typeof input == 'string' ) {
       var new_path = this.state.path;
       new_path.push(input);
-      this.setState({ path: new_path });
+      this.navigateTo( new_path );
     }
     this.showContent();
   }; // end navClick
 
+  goUp() {
+    var new_path = this.state.path;
+    new_path.pop();
+    this.navigateTo( new_path );
+  }; // end goUp
+
   handleBackBtn() {
     if ( this.state.path.length < 2 ) {
-      var new_path = this.state.path;
-      new_path.pop();
-      this.setState({ path: new_path });
+      this.goUp();
     } else {
-      this.setState({ path: this.state.path.slice(0,1) });
+      this.navigateTo( this.state.path.slice(0,1) );
     }
     this.showContent();
   }; // end handleBackBtn
 
   handleHomeBtn() {
-    this.setState({ path: [] });
+    this.navigateTo( [] );
     this.showContent();
   }; // end handleHomeBtn
 
-  render() {
-    var curr_node = this.getNode(UI_MAP,this.state.path);
+  submitCurrentLeaf() {
+    var new_leaves = this.state.leaves;
+    var node = this.getNode(UI_MAP,this.state.path);
+    new_leaves.push({
+      code: node.leafcode, data: this.state.data
+    });
+    this.handleBackBtn();
+  }; // end submitCurrentLeaf
+
+  saveData(data) {
+    console.log('saving data: '+data);
+    var new_data = this.state.data;
+    new_data[ this.state.path.join(':') ] = data;
+    this.setState({ data: new_data });
+    console.log(JSON.stringify(this.state));
+  }; // end saveData
+
+  renderLeafConfirmation() {
+    var title = 'Would you like to submit the following leaf?';
+    var message = this.state.path.join(' : ');
+    var node = this.getNode(UI_MAP,this.state.path);
+    message += ' -> ' + node.leafcode;
+    var data = !node.input || this.state.data[ this.state.path.join(':') ];
+    return (
+      <AlertPopup
+        className='leaf-alert'
+        type='confirm'
+        title={title}
+        message={message}
+        onSubmit={() => this.hideContentCallback(this.submitCurrentLeaf,[])}
+        onCancel={this.goUp}
+        mounted={data && node.leafcode != undefined}
+        transition='bottom'
+      />
+    );
+  }; // end renderLeafConfirmation
+
+  renderTextInputBox() {
+    var message = 'This input requires text:';
+    var text_prompt = 'text goes here';
+    var node = this.getNode(UI_MAP,this.state.path);
+    var data = node.input && !this.state.data[ this.state.path.join(':') ];
+    return (
+      <InputPopup
+        className='text-input-alert'
+        type='textinput'
+        message={message}
+        text_prompt={text_prompt}
+        onSubmit={this.saveData}
+        transition='right'
+        mounted={data}
+      />
+    );
+  }; // end renderTextInputBox()
+
+  renderTitles() {
+    var node = this.getNode(UI_MAP,this.state.path);
     var section_label = this.state.path.length > 0 ? 
       this.getNode(UI_MAP,this.state.path.slice(0,1)).label : '';
     var type_label = this.state.path.length > 1 ?
       this.getNode(UI_MAP,this.state.path.slice(0,2)).label : '';
     var title = 'Select Section';
     var subtitle = '';
-    var nav_panel_style = 'block';
     if ( this.state.path.length > 0 ) {
       title = 'Select Note Type';
-      subtitle = curr_node.label;
-      nav_panel_style = 'mega-list';
+      subtitle = node.label;
     }
     if ( this.state.path.length > 1 ) {
       title = 'Build Note';
       subtitle = section_label + ' - ' + type_label;
+    }
+    return [
+      <h1 key='main-title' className='main-title'>{title}</h1>,
+      <h2 key='subtitle' className='sub-title'>{subtitle}</h2>
+    ];
+  }; // end renderTitles
+
+  renderNavPane() {
+    var node = this.getNode(UI_MAP,this.state.path);
+    var nav_panel_style = 'block';
+    if ( this.state.path.length > 0 ) {
+      nav_panel_style = 'mega-list';
+    }
+    if ( this.state.path.length > 1 ) {
       nav_panel_style = 'accordion';
-      curr_node = this.getNode(UI_MAP,this.state.path.slice(0,2));
+      node = this.getNode(UI_MAP,this.state.path.slice(0,2));
     }
     return (
+      <NavPanel
+        style={nav_panel_style}
+        option_tree={node}
+        onClick={(key) => this.hideContentCallback(this.navClick,[key])} 
+        clickNoUIUpdate={this.navClick}
+        show_children={this.state.show_content}
+      />
+    );
+  }; // end renderNavPane
+
+  render() {
+    return (
       <div className='main-page'>
-        <h1 className='main-title'>{title}</h1>
-        <h2 className='sub-title'>{subtitle}</h2>
-        <NavPanel
-          style={nav_panel_style}
-          option_tree={curr_node}
-          onClick={(key) => this.hideContentCallback(this.navClick,[key])} 
-          clickNoUIUpdate={this.navClick}
-          show_children={this.state.show_content}
-        />
+        {this.renderTitles()}
+        {this.renderNavPane()}
         <FancyButton className='menu-btn' icon='list' direction='left' mounted='true' />
         <FancyButton className='search-btn' icon='search' direction='right' mounted='true' />
         <FancyButton className='exit-btn' icon='save' direction='right' mounted={this.state.leaves.length > 0} />
@@ -130,6 +235,8 @@ class MainPage extends React.Component {
           onClick={() => this.hideContentCallback(this.handleHomeBtn,[])}
           mounted={this.state.path.length > 1}
         />
+        {this.renderLeafConfirmation()}
+        {this.renderTextInputBox()}
       </div>
     );
   }; // end render
